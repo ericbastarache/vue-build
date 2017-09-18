403bfe9c6cc0b36a8911af63f07b9eba3c422c3d
Eric Bastarache <eric_bastarache@hotmail.com>
2017-09-17 20:33:40 -0400

    preliminary webpack set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 };
